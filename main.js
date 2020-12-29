3dba1494eb396690cf003af9ff7dd1a7b4a5db98
Wladys LELONG <wladys.lelong@gmail.com>
2020-12-29 18:24:06 +0100

    Requete POST fonctionnelle

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const urlApi = "http://localhost:3000/api/cameras";
 let camerasArray=""; 
-const camerasContainer = document.querySelector('#cameras-container');
-const carouselContainer = document.querySelector('#carousel-container');
+const camerasContainer = document.querySelector('#camera_Container');
+const recapCommande = document.querySelector('#recap_commande');
+const totalElement = document.getElementsByClassName("total");
 const badgePanier = document.querySelector('#badge_panier');
+
 
 class elementPanier {
     constructor(name, id, option, quantity, price){
@@ -16,7 +18,8 @@ class elementPanier {
 
 let elementsPanier = [];
 
-// crée la requete
+console.log(camerasContainer);
+
 let requete = new XMLHttpRequest;
 requete.open('GET', urlApi);
 requete.responseType = 'json'; 
@@ -28,52 +31,17 @@ requete.onload = function () {
         if (requete.status === 200 ||requete.status === 201){
             camerasArray = requete.response; // stocke la reponse dans une variable.
             console.log(camerasArray);
+            // on cherche la bonne camera dans le array.
             majContenuPanier();
             afficherBadgePanier();
-            afficherCameras(); // affiche dynamiquement les cameras en utilisant la fonction.
-            }
+            formCheck();
+        }
         
     } else {
         alert("Un problème est survenu, merci de réessayer plus tard");
     }
- }
-
-// creation de la fonction pour afficher les cameras. 
-function afficherCameras() {
-    camerasArray.forEach(afficherCamera);
-    camerasArray.forEach(afficherCarousel);
-    const firstCarouselElement = document.querySelector("#carousel-container .carousel-item");
-    firstCarouselElement.classList.add("active"); 
 }
-
-function afficherCarousel(camera) {
-    const carouselElement = document.createElement('div');
-    carouselElement.setAttribute("class","carousel-item")
-    carouselElement.innerHTML = `<img src="${camera.imageUrl}" class="d-block w-100" alt="${camera.name}">`;
-    carouselContainer.appendChild(carouselElement);
-}
-
-// creation de la fonction pour afficher une camera
- function afficherCamera(camera) {
-     const cameraElement = document.createElement('div');  
-     cameraElement.setAttribute("class","ccol-12 col-lg-4")
-     cameraElement.innerHTML = 
-         `<div class="card mt-5 mb-4 mb-lg-0">
-            <img src="${camera.imageUrl}" alt="${camera.name}" class="card-img-top" >
-            <span class="position-absolute mr-3 mt-3 justify-content-end float-right badge btn-success" style="right: 0">New</span>
-            <div class="card-body">
-                <div class="row mb-3">
-                    <h5 class="card-title mb-0 col-10 align-middle">${camera.name}</h5>
-                    <span class="badge badge-pill badge-secondary col-2 align-self-center py-2">${camera.price/100} €</span>
-                </div>
-                <p class="card-text">${camera.description}</p>
-                <button type="button" class="btn btn-secondary text-muted btn-lg btn-block"><a class="text-decoration-none text-white stretched-link" href="produit.html?${camera._id}">Commander</a></button>
-            </div>
-        </div>`;
-    camerasContainer.appendChild(cameraElement);
- }
-
-
+            
 function majContenuPanier() {
     for (let i=0; i < camerasArray.length; i++){
         for (let j=0; j < camerasArray[i].lenses.length; j++){
@@ -94,7 +62,7 @@ function majContenuPanier() {
     }
     console.log(elementsPanier);
     return elementsPanier;
-}
+}            
 
 function afficherBadgePanier() {
     badgePanier.innerHTML=`${elementsPanier.length}`;
---
+++ panierb.js
@@ -0,0 +1,176 @@
+const urlApiGET = "http://localhost:3000/api/cameras";
+const urlApiPOST = "http://localhost:3000/api/cameras/order";
+var camerasArray=""; 
+const camerasContainer = document.querySelector('#camera_Container');
+const recapCommande = document.querySelector('#recap_commande');
+const totalElement = document.getElementsByClassName("total");
+const badgePanier = document.querySelector('#badge_panier');
+
+
+class elementPanier {
+    constructor(name, id, option, quantity, price){
+        this.name = name;
+        this.id = id;
+        this.option = option;
+        this.quantity = quantity;
+        this.price = price;
+    }
+}
+
+var elementsPanier = [];
+
+console.log(camerasContainer);
+
+var requete = new XMLHttpRequest;
+requete.open('GET', urlApiGET);
+requete.responseType = 'json'; 
+requete.send(); // envoie la requete
+
+// Dès qu'on reçoit la réponse, on execute la fonction:
+requete.onload = function () { 
+    if (requete.readyState === XMLHttpRequest.DONE ) { // verifie l'état de la requête. 
+        if (requete.status === 200 ||requete.status === 201){
+            camerasArray = requete.response; // stocke la reponse dans une variable.
+            console.log(camerasArray);
+            // on cherche la bonne camera dans le array.
+            majContenuPanier();
+            afficherBadgePanier();
+            animationInput();
+            //prevent();
+        }
+        
+    } else {
+        alert("Un problème est survenu, merci de réessayer plus tard");
+    }
+}
+            
+function majContenuPanier() {
+    for (let i=0; i < camerasArray.length; i++){
+        for (let j=0; j < camerasArray[i].lenses.length; j++){
+            if (localStorage.getItem("qte_"+camerasArray[i]._id+"/"+camerasArray[i].lenses[j]) != null && localStorage.getItem("qte_"+camerasArray[i]._id+"/"+camerasArray[i].lenses[j])>=0) {
+                let localName=camerasArray[i].name;
+
+                let localId=camerasArray[i]._id;
+                //console.log(localId);
+                let localOption = camerasArray[i].lenses[j];
+                //console.log(localOption);
+                let localQuantity = localStorage.getItem("qte_"+camerasArray[i]._id+"/"+camerasArray[i].lenses[j])
+                //console.log(localQuantity);
+                let localPrice = camerasArray[i].price;
+                
+                elementsPanier.push(new elementPanier(localName, localId, localOption, localQuantity, localPrice));
+            }
+        }
+    }
+    console.log(elementsPanier);
+    return elementsPanier;
+}            
+
+function afficherBadgePanier() {
+    badgePanier.innerHTML=`${elementsPanier.length}`;
+}
+
+//function prevent(){
+//    const btnValider = document.getElementById("bouton-validation");
+//    btnValider.addEventListener("click", function(event){
+//        formCheck();
+//        event.preventDefault();
+//        event.stopPropagation();
+//
+//    });
+//}
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+function animationInput() {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation');
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+            //formCheck();
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+}
+
+function formCheck() {
+    const form = document.querySelector('.needs-validation');
+    // si la valeur du champ prenom est non vide
+    const orderInput = form.getElementsByTagName('input');
+    console.log(orderInput);
+    
+    for (let i=0; i < orderInput.length; i++){
+       if (orderInput[i].required){
+           if(orderInput[i].value && orderInput[i].validity.valid){
+                //alert(orderInput[i].value+" "+orderInput[i].validity.valid);
+            } else {
+                alert("Merci de remplir correctement le "+ orderInput[i].name + " ou de vérifier la conformité avec le format attendu!!")
+                return false;
+            }
+       } 
+    }
+    const contact = {
+      firstName: orderInput[0].value,
+      lastName: orderInput[1].value,
+      address: orderInput[2].value,
+      city: orderInput[4].value,
+      email: orderInput[6].value
+    }
+    
+    console.log(contact);
+    
+
+    let products = [];
+    for (let i=0; i < elementsPanier.length; i++){
+        alert(elementsPanier[i].id);
+        products.push(elementsPanier[i].id);
+    }
+    alert(products);
+    let order = { contact, products };
+    console.log(order);
+    alert(order);
+
+
+    // requete post 
+    var request = new XMLHttpRequest();
+    request.open("POST", urlApiPOST);
+    request.setRequestHeader("Content-Type", "application/json");
+    request.send(JSON.stringify(order));
+    request.responseType = 'json'; 
+    
+    alert("début");
+    //reponse de la requete
+    request.onload = function () { 
+        if (request.readyState === XMLHttpRequest.DONE ) { // verifie l'état de la requête. 
+            if (request.status === 200 ||request.status === 201){
+                alert(request.response);
+                console.log(request.response);
+                let confirmation = request.response;
+                console.log(confirmation);
+                let idConfirmation = confirmation.orderId;
+                alert(idConfirmation);
+                console.log(idConfirmation);
+
+                localStorage.setItem("confirm", idConfirmation);
+                //localStorage.removeItem("");
+            }
+            else {
+                console.log(error);
+                alert("Un problème est survenu, merci de réessayer plus tard");
+           }
+        }
+    }
+        
+    alert("fin");
+    return true;
+    
+}
